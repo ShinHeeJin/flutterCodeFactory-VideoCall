9ecaddbb764656240304a84258c1efb0ab74c22d
ShinHeeJin <sisesu92@gmail.com>
2023-03-28 00:46:22 +0900

    feat : agora .env

---
+++ lib/const/agora.dart
@@ -0,0 +1,4 @@
+const APP_ID = "ffb2cea783da428faee08fe61c77787b";
+const CHANNEL_NAME = "testchannel-heej";
+const TEMP_TOKEN =
+    "007eJxTYHhyXqhU9HHnia7MV4ejE+2/vFE+Uvrb+3v831mHih0Kn+cpMKSlJRklpyaaWxinJJoYWaQlpqYaWKSlmhkmm5ubW5gnrd6pmNIQyMhwxy+YkZEBAkF8AYaS1OKS5IzEvLzUHN2M1NQsBgYAT/0oHA==";
