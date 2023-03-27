2b1add792dd75c5e0b8e639c4dfb2278debe2fd5
ShinHeeJin <sisesu92@gmail.com>
2023-03-28 01:29:45 +0900

    feat : Logo

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -26,7 +26,45 @@ class _Logo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue[300]!,
+              blurRadius: 12.0,
+              spreadRadius: 2.0,
+            )
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Icon(
+                Icons.videocam,
+                color: Colors.white,
+                size: 40.0,
+              ),
+              SizedBox(
+                width: 12.0,
+              ),
+              Text(
+                "LIVE",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  letterSpacing: 4.0,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
