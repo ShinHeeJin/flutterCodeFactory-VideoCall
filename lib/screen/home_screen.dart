cf443f0b6aae508a3504556708b4ebdab5c0755e
ShinHeeJin <sisesu92@gmail.com>
2023-03-28 01:31:31 +0900

    feat : _Image

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -73,7 +73,11 @@ class _Image extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Image.asset(
+        "asset/img/home_img.png",
+      ),
+    );
   }
 }
 
