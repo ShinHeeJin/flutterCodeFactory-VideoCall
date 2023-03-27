61d0b070305991e592cdae4215df8b3decf412b1
ShinHeeJin <sisesu92@gmail.com>
2023-03-28 01:23:54 +0900

    feat : LayOut

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,10 +5,45 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        "Home Screen",
+    return Scaffold(
+      backgroundColor: Colors.blue[100]!,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: const [
+            Expanded(child: _Logo()),
+            Expanded(child: _Image()),
+            Expanded(child: _EntryButton()),
+          ]),
+        ),
       ),
     );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class _Image extends StatelessWidget {
+  const _Image();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class _EntryButton extends StatelessWidget {
+  const _EntryButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
