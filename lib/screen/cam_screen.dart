44915385c51a0277041e2be07fcd65f844ef1edf
ShinHeeJin <sisesu92@gmail.com>
2023-03-28 01:43:56 +0900

    feat : Permission ( camera, microphone )

--- lib/screen/cam_screen.dart
+++ lib/screen/cam_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class CamScreen extends StatefulWidget {
   const CamScreen({super.key});
@@ -8,6 +9,20 @@ class CamScreen extends StatefulWidget {
 }
 
 class _CamScreenState extends State<CamScreen> {
+  Future<bool> init() async {
+    final resp = await [Permission.camera, Permission.microphone].request();
+
+    final cameraPermission = resp[Permission.camera];
+    final micPermission = resp[Permission.microphone];
+
+    if (cameraPermission != PermissionStatus.granted ||
+        micPermission != PermissionStatus.granted) {
+      throw "카메라 또는 마이크 권한이 없습니다.";
+    }
+
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
